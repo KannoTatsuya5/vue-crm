624dcc393ddada250fd294f3f0a8d65fe9e892c1
KannoTatsuya5 <1998tatsuya0513@gmail.com>
2024-01-31 08:53:22 +0900

    lang等設定 & vue

---
+++ resources/js/Pages/Inertia/Create.vue
@@ -0,0 +1,30 @@
+<script setup>
+    import { Inertia } from '@inertiajs/inertia'
+    import { reactive  } from 'vue';
+
+    defineProps({
+        errors: Object
+    });
+
+    const form = reactive ({
+        title:null,
+        content:null
+    })
+
+    const submitFunction = () => {
+        Inertia.post('/inertia/store', form);
+    }
+
+</script>
+
+<template>
+    <form @submit.prevent="submitFunction">
+        <input type="text" name="title" v-model="form.title">
+        <br>
+        <div class="text-red-600" v-if="errors.title">{{ errors.title }}</div>
+        <input type="text" name="content" v-model="form.content">
+        <br>
+        <div class="text-red-600" v-if="errors.content">{{ errors.content }}</div>
+        <button>送信</button>
+    </form>
+</template>
---
+++ resources/js/Pages/Inertia/Show.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { Inertia } from '@inertiajs/inertia';
+defineProps({
+    id : String,
+    blog : Object
+})
+
+const deleteConfirm = (id) => {
+    Inertia.delete(`/inertia/${id}`, {
+        onBefore: () => confirm('本当に削除しますか')
+    })
+}
+</script>
+
+<template>
+    {{ id }}<br>
+    {{ blog.tittle }}<br>
+    <button class="bg-blue-500 p-3 m-3" type="button" @click="deleteConfirm(blog.id)">削除</button>
+</template>
